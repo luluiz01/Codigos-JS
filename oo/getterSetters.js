cf84146274130074ea7a60f1507e2f2a5ec5f42d
Luiz Henrique <luiz.pventura@gmail.com>
2020-03-01 08:48:57 -0300

    Henraça

---
+++ oo/getterSetters.js
@@ -0,0 +1,16 @@
+const sequencia = {
+    _valor1: 1, //convenção
+    get valor() { return this._valor1++ },
+    set valor(valor) {
+        if (valor > this._valor1) {
+            this._valor1 = valor
+        }
+    }
+}
+
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 1000
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 999
+console.log(sequencia.valor, sequencia.valor)
+
