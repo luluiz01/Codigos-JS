e04610d02d10c6dc758d53c6116fc446d53df328
Luiz Henrique <luiz.pventura@gmail.com>
2020-03-05 08:47:55 -0300

    filter

---
+++ array/map1.js
@@ -0,0 +1,12 @@
+const nums = [1, 2, 3, 4, 5]
+//map é For com um propósito
+let resultado = nums.map(e => e * 2)
+//console.log(nums)
+console.log(resultado)
+
+const somar10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+resultado = nums.map(somar10).map(triplo).map(paraDinheiro)
+console.log(resultado)
